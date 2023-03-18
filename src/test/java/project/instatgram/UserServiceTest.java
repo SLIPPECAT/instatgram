c58fb6c873d31a248145ca00996c7253924e6e2e
SLIPPECAT <119715555+SLIPPECAT@users.noreply.github.com>
2023-03-18 22:18:15 +0900

    Cat (#15)
    * feat: UserRequestDto 추가
    
    * feat: signup, login 기능 구현
    
    * feat: signup, login 기능 구현 + 예외처리
    
    * feat: signup, login 기능 구현 + 예외처리
    
    * first commit (#5)
    
    회원가입, 로그인, 댓글 기능 구현
    
    * feat: 3월 17일 금요일
    
    * feat: signup, login 기능 구현 + 예외처리
    
    * feat: 기존 기능 + DB살아있는 거 확인(배포까지 해보자!)
    
    * feat: UserRequestDto 추가
    
    * feat: signup, login 기능 구현
    
    * feat: signup, login 기능 구현 + 예외처리
    
    * feat: signup, login 기능 구현 + 예외처리
    
    * first commit (#5)
    
    회원가입, 로그인, 댓글 기능 구현
    
    * feat: 3월 17일 금요일
    
    * feat: signup, login 기능 구현 + 예외처리
    
    * feat: 기존 기능 + DB살아있는 거 확인(배포까지 해보자!)
    
    * feat: 기존 기능 + DB살아있는 거 확인(배포까지 해보자!)
    
    * feat: 테스트 커밋
    
    * feat: 테스트 커밋
    
    * feat: 테스트 커밋
    
    * feat: 테스트 커밋
    
    * feat: 테스트 커밋
    
    * feat: 테스트 커밋
    
    * feat: 테스트 커밋
    
    ---------
    
    Co-authored-by: mac <ryjun91@github.com>

--- src/test/java/project/instatgram/UserServiceTest.java
+++ src/test/java/project/instatgram/UserServiceTest.java
@@ -1,30 +1,7 @@
 package project.instatgram;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import project.instatgram.entity.User;
-import project.instatgram.entity.UserRoleEnum;
-import project.instatgram.repository.UserRepository;
-
 @SpringBootTest
 public class UserServiceTest {
 
-    UserRepository userRepository;
-
-    @Test
-    @DisplayName("회원가입 기능 테스트")
-    public void signup(){
-        // given
-        String username = "아이유";
-        String password = "너랑나";
-        String nickname = "이지은";
-        // when
-        User user = new User(username, password, nickname, UserRoleEnum.USER);
-        userRepository.save(user);
-        // then
-
     }
-
-
 }

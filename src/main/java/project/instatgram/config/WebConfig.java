b24b7a6cee55aebb8c7eb11113d22fcb8934200f
mac <ryjun91@github.com>
2023-03-18 20:59:56 +0900

    feat: 테스트 커밋

--- src/main/java/project/instatgram/config/WebConfig.java
+++ src/main/java/project/instatgram/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://100.27.14.51")
+                .allowedOrigins("http://18.233.63.248/")
                 .allowedMethods("*")
                 .allowCredentials(false)
                 .maxAge(3000);

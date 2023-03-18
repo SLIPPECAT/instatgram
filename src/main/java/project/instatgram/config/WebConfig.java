6bf8a5b20a4b6159a29dd0599f04ebbf972a62c6
mac <ryjun91@github.com>
2023-03-18 18:05:36 +0900

    feat: signup, login 기능 구현 + 예외처리

---
+++ src/main/java/project/instatgram/config/WebConfig.java
@@ -0,0 +1,19 @@
+package project.instatgram.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://100.27.14.51")
+                .allowedMethods("*")
+                .allowCredentials(false)
+                .maxAge(3000);
+    }
+}

a0cf26de3cff163344364ce586cf559d16b13799
SLIPPECAT <119715555+SLIPPECAT@users.noreply.github.com>
2023-03-20 19:50:14 +0900

    Update WebConfig.java

--- src/main/java/project/instatgram/config/WebConfig.java
+++ src/main/java/project/instatgram/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://54.157.187.147/**")
+                .allowedOrigins("http://13.124.243.93//**")
                 .allowedMethods("*")
                 .allowCredentials(false)
                 .maxAge(3000);

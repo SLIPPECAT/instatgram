f95681a0eca4c3ae5d780f12f704e262982cf3aa
mac <ryjun91@github.com>
2023-03-21 23:59:49 +0900

    "30. IDENTITY"

--- src/main/java/project/instatgram/entity/Post.java
+++ src/main/java/project/instatgram/entity/Post.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class Post extends Timestamped{
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String title;

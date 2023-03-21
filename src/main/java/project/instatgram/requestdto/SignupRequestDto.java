ea94c5e9fdc19db96b600bc368751d6467a1d274
mac <ryjun91@github.com>
2023-03-21 20:53:24 +0900

    "20. 주석하고 커밋"

--- src/main/java/project/instatgram/requestdto/SignupRequestDto.java
+++ src/main/java/project/instatgram/requestdto/SignupRequestDto.java
@@ -10,9 +10,9 @@ import javax.validation.constraints.Pattern;
 @NoArgsConstructor
 public class SignupRequestDto {
 
-    @NotBlank(message = "이메일을 입력해주세요.")
-    @Pattern(regexp = "^[A-Za-z0-9_\\.\\-]+@[A-Za-z0-9\\-]+\\.[A-Za-z0-9\\-]+$",
-            message = "이메일 형식으로 작성해주세요.")
+//    @NotBlank(message = "이메일을 입력해주세요.")
+//    @Pattern(regexp = "^[A-Za-z0-9_\\.\\-]+@[A-Za-z0-9\\-]+\\.[A-Za-z0-9\\-]+$",
+//            message = "이메일 형식으로 작성해주세요.")
     private String username;
 
     @NotBlank(message = "비밀번호를 입력해주세요.")

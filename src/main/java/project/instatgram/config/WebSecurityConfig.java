56dc397e4eb613ac62fb6917a1172667a2513aab
mac <ryjun91@github.com>
2023-03-22 19:20:42 +0900

    "16. 정규식 주석 수정"

--- src/main/java/project/instatgram/config/WebSecurityConfig.java
+++ src/main/java/project/instatgram/config/WebSecurityConfig.java
@@ -48,10 +48,8 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
-
 // 기본 설정인 Session 방식은 사용하지 않고 JWT 방식을 사용하기 위한 설정
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
         http.authorizeRequests()
                 .antMatchers("/**").permitAll()
                 .antMatchers("/api/**").permitAll()
--- src/main/java/project/instatgram/requestdto/SignupRequestDto.java
+++ src/main/java/project/instatgram/requestdto/SignupRequestDto.java
@@ -12,16 +12,16 @@ import javax.validation.constraints.Pattern;
 public class SignupRequestDto {
 
 
-    @NotBlank(message = "이메일 형식을 맞추서 입력해주세요.")
-    @Pattern(regexp = "^[a-zA-Z0-9]+@[a-zA-Z0-9.-]+\\.[a-zA-Z0-9]+$", message = "이메일 형식을 맞추서 입력해주세요.")
+//    @NotBlank(message = "이메일 형식을 맞추서 입력해주세요.")
+//    @Pattern(regexp = "^[a-zA-Z0-9]+@[a-zA-Z0-9.-]+\\.[a-zA-Z0-9]+$", message = "이메일 형식을 맞추서 입력해주세요.")
     private String username;
 
-    @NotBlank(message = "비밀번호를 입력해주세요.")
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\\-={}\\[\\]\\|:;\"'<>,.?\\/]).{4,15}$",
-            message = "4~15글자, 글자 1개, 숫자 1개, 특수문자 1개 꼭 입력해야합니다.")
+//    @NotBlank(message = "비밀번호를 입력해주세요.")
+//    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\\-={}\\[\\]\\|:;\"'<>,.?\\/]).{4,15}$",
+//            message = "4~15글자, 글자 1개, 숫자 1개, 특수문자 1개 꼭 입력해야합니다.")
     private String password;
 
-    @NotBlank(message = "닉네임을 입력해주세요.")
+//    @NotBlank(message = "닉네임을 입력해주세요.")
     private String nickname;
 
 

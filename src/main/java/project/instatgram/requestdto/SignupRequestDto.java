4de90be3992ce11b64f020505482ae4515dfd518
mac <ryjun91@github.com>
2023-03-22 16:39:58 +0900

    "10. 정규식 마무리? validated는 입력값에!"

--- src/main/java/project/instatgram/requestdto/SignupRequestDto.java
+++ src/main/java/project/instatgram/requestdto/SignupRequestDto.java
@@ -7,7 +7,6 @@ import org.springframework.validation.annotation.Validated;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
-@Validated
 @Getter
 @NoArgsConstructor
 public class SignupRequestDto {

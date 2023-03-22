c5ff555085cc9911194349c378da5419a4344418
mac <ryjun91@github.com>
2023-03-22 20:35:13 +0900

    "21. 코멘트 생성자 추가"

--- src/main/java/project/instatgram/dto/CommentResponseDto.java
+++ src/main/java/project/instatgram/dto/CommentResponseDto.java
@@ -9,12 +9,11 @@ import java.time.LocalDateTime;
 public class CommentResponseDto {
     private Long id;
     private String comment;
-
     private String nickname;
-
     private LocalDateTime createdAt;
-
     private LocalDateTime modifiedAt;
+    private Long postId;
+
 
 
     public CommentResponseDto(Comment comment) {
@@ -24,6 +23,14 @@ public class CommentResponseDto {
         this.createdAt = comment.getCreatedAt();
         this.modifiedAt = comment.getModifiedAt();
 
+    }
 
+    public CommentResponseDto(Comment comment, Long postId) {
+        this.id = comment.getId();
+        this.comment = comment.getComment();
+        this.nickname = comment.getUser().getNickname();
+        this.createdAt = comment.getCreatedAt();
+        this.modifiedAt = comment.getModifiedAt();
+        this.postId = postId;
     }
 }

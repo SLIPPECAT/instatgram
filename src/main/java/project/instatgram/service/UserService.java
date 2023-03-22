1cc1b31f44bf3f7d5b2a562a778a98afecc35287
SLIPPECAT <119715555+SLIPPECAT@users.noreply.github.com>
2023-03-22 14:54:55 +0900

    Update UserService.java

--- src/main/java/project/instatgram/service/UserService.java
+++ src/main/java/project/instatgram/service/UserService.java
@@ -53,7 +53,6 @@ public class UserService {
     public void login(LoginRequestDto requestDto, HttpServletResponse response){
         String username = requestDto.getUsername();
         String password = requestDto.getPassword();
-
         User user = userRepository.findByUsername(username)
                 .orElseThrow(()->new IllegalArgumentException("아이디가 일치하지 않습니다."));
         // 저장된 암호와 입력왼 암호 비교
@@ -61,6 +60,7 @@ public class UserService {
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
         StatusResponseDto statusResponseDto = new StatusResponseDto(HttpStatus.OK.value(), "로그인 완료");
+        // Jwt 토큰 발급
         response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(user.getUsername(), user.getRole()));
     }
 }

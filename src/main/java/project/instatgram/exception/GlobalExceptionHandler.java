75e845cc13b354b32c4ef8e5ad396939999be2e7
SLIPPECAT <119715555+SLIPPECAT@users.noreply.github.com>
2023-03-23 19:40:22 +0900

    Last2 (#41)
    * "14. 유저 서비스 반환값 변경"
    
    * "15. 주석 삭제"
    
    ---------
    
    Co-authored-by: mac <ryjun91@github.com>

--- src/main/java/project/instatgram/exception/GlobalExceptionHandler.java
+++ src/main/java/project/instatgram/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -13,24 +14,4 @@ public class GlobalExceptionHandler {
         log.error("handleCustomException throw CustomException : {}", e.getErrorCode());
         return ErrorResponse.toResponseEntity(e.getErrorCode());
     }
-
-//    @ExceptionHandler({IllegalArgumentException.class})
-//    protected ResponseEntity<Object> illegalArgumentException(IllegalArgumentException e){
-//        StatusResponseDto statusResponseDto = new StatusResponseDto();
-//        statusResponseDto.setStatus(HttpStatus.BAD_REQUEST.value());
-//        statusResponseDto.setMsg(e.getMessage());
-//        return new ResponseEntity<>(statusResponseDto, HttpStatus.BAD_REQUEST);
-//    }
-
-//    @ExceptionHandler({JwtUnvalidException.class})
-//    protected ResponseEntity<Object> JwtUnvalidException(JwtUnvalidException e){
-//        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
-//    }
-
-//    @ExceptionHandler({SignatureException.})
-
-//    @ExceptionHandler({Exception.class})
-//    protected ResponseEntity<Object> handleServerException(Exception e) {
-//        return new ResponseEntity<>(new ErrorDto(TOKEN_SECURITY_ERROR.getStatus(), TOKEN_SECURITY_ERROR.getMsg()), HttpStatus.INTERNAL_SERVER_ERROR);
-//    }
 }

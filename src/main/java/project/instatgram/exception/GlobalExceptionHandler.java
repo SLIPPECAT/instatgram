f4e215deddbf8d897929167dd0077a4073835ba2
mac <ryjun91@github.com>
2023-03-23 20:06:52 +0900

    "16. illegal 예외 "

--- src/main/java/project/instatgram/exception/GlobalExceptionHandler.java
+++ src/main/java/project/instatgram/exception/GlobalExceptionHandler.java
@@ -1,9 +1,11 @@
 package project.instatgram.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import project.instatgram.responsedto.StatusResponseDto;
 
 @Slf4j
 @RestControllerAdvice
@@ -13,5 +15,13 @@ public class GlobalExceptionHandler {
     protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
         log.error("handleCustomException throw CustomException : {}", e.getErrorCode());
         return ErrorResponse.toResponseEntity(e.getErrorCode());
+    }
+
+    @ExceptionHandler({IllegalArgumentException.class})
+    protected ResponseEntity<Object> illegalArgumentException(IllegalArgumentException e){
+        StatusResponseDto statusResponseDto = new StatusResponseDto();
+        statusResponseDto.setStatus(HttpStatus.BAD_REQUEST.value());
+        statusResponseDto.setMsg(e.getMessage());
+        return new ResponseEntity<>(statusResponseDto, HttpStatus.BAD_REQUEST);
     }
 }
